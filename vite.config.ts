b1ee1e04675265f5c7afd93a5c4a1fafe59ea5df
maxarvid <max.arvid.anderson@gmail.com>
2022-10-01 16:19:35 +0200

    adds vite config to remove data-cy attrs from build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const removeDataTestAttrs = (node) => {
+  if (node.type === 1) {
+    node.props = node.props.filter(prop => prop.type === 6 ? prop.name !== 'data-cy' : true)
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        nodeTransforms: process.env.NODE_ENV === 'production' ? [removeDataTestAttrs] : []
+      }
+    }
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

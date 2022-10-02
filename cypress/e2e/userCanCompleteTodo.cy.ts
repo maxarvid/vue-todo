904bade8dcb055250e7be452cce67623978d9efd
maxarvid <max.arvid.anderson@gmail.com>
2022-10-02 11:16:35 +0200

    🧪 adds Todo completion test

---
+++ cypress/e2e/userCanCompleteTodo.cy.ts
@@ -0,0 +1,18 @@
+describe('When user intends to complete a Todo', () => {
+  beforeEach(() => {
+    cy.visit('/', {
+      onBeforeLoad: () => {
+        localStorage.setItem('toDos', JSON.stringify({ text: 'Get groceries', id: '1', completed: false }))
+      }
+    })
+  });
+
+  it('is expected to render a completion icon', () => {
+    cy.get("[data-cy=todo-icons]").children().first().should('be.visible')
+  });
+
+  it('is expected to put a strikethrough on Todo upon click', () => {
+    cy.get("[data-cy=todo-icons]").children().first().click()
+    cy.get("[data-cy=todo-list]").children().first().should('have.css', 'text-decoration: line-through')
+  });
+});
